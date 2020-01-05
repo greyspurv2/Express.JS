388f6c50d0bb4d296c58077d065bd91b5d58d036
greyspurv2 <35030409+greyspurv2@users.noreply.github.com>
2020-01-06 00:13:34 +0100

    Add files via upload
    Fun with Express for my own learning and amusement

---
+++ server.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+const express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("<h1>Hello world!</h1><br><h2> Welcome to the site </h2>");
+});
+
+app.get("/contact", function(req, res) {
+    res.send("Contact me at this@email.com")
+});
+
+app.get("/development", function(req, res) {
+    res.send("<ul><li>Flutter</ul></li><ul><li>JavaScript</ul></li><ul><li>Node.JS</ul></li>")
+});
+
+app.get("/about", function(req, res) {
+    res.send("this company is about..")
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 2020");
+});
